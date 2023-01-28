112f1214abf4e0c9e67f3f8a3e8e22f232ad4f35
Julien <julien.quennehen@edu.devinci.fr>
2023-01-29 00:36:29 +0100

    feat: 🎸 standing file date function and rewrite fetch function

--- src/components/utils/FetchNewRanks/index.mjs
+++ src/components/utils/FetchNewRanks/index.mjs
@@ -1,13 +1,9 @@
 import https from "https"
-import path, { dirname } from "path"
-import fs, { readFileSync, writeFileSync } from "fs"
+import path from "path"
+import fs, { readFileSync} from "fs"
 import dateFormat from "dateformat"
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename) + "/standings";
 const regions = JSON.parse(readFileSync("./regions.json", "utf-8"))
-
 var objMap = new Map(Object.entries(regions));
 
 objMap.forEach((url, standing_name) => {
@@ -15,11 +11,7 @@ objMap.forEach((url, standing_name) => {
 });
 
 function downloadFile(url, standing_name) {
-    const file = path.basename(url)
-    const filename = `${__dirname}/${standing_name}/${dateFormat(Date(), "dd-mm-yy")}.md`
-    const filePath = path.resolve(`./standings/${standing_name}/${dateFormat(Date(), "dd-mm-yy")}.md`);
-    console.log(filePath)
-    console.log(file)
+    const filename = `../standings/${standing_name}/${dateFormat(Date(), "dd-mm-yy")}.md`
     https.get(url, (res) => {
         res.setEncoding('utf8');
         res.on('data', function(chunk) {
